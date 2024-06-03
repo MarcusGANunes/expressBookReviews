dfe1c31ba2413c154ae46b526577792386386ea8
Marcus Nunes <marcusnunes_11@hotmail.com>
2024-06-03 15:11:50 -0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,7 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).json({message: JSON.stringify(books)});
+  return res.status(200).json(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
